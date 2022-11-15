bd9033c4d2530e5edb7a4828da4363247149bf5f
tarangjivrajani <tarang.jivrajani@gmail.com>
2022-11-15 13:38:39 +0530

    Pattern Code

---
+++ patterns.py
@@ -0,0 +1,18 @@
+n=int(input('Enter a value: '))
+for i in range(n):
+      print('*'*(i+1))
+
+
+n=int(input('Enter a value: '))
+for i in range(n):
+    print('*'*(n-i))
+
+
+n=int(input('Enter a value: '))
+for i in range(n):
+    print(' '*(n-i) ,' *'*(i+1) )
+
+n=int(input('Enter a value: '))
+for i in range(n):
+    print(' '*(i) ,' *'*(n-i))
+
